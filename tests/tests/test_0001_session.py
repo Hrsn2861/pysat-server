d7c784a513e65d4a732230de56cb24d868a81cce
陈浩展 <chz17@mails.tsinghua.edu.cn>
2019-10-17 02:41:09 +0800

    test: test for session

--- tests/tests/test_0001_session.py
+++ tests/tests/test_0001_session.py
@@ -16,16 +16,7 @@ class TestSession(TestCase):
     Test session for pysat server session
     """
 
-    def test_0001_startsession(self):
-        """
-        Check start session
-        """
-        response = self.client.post('/session/start')
-        self.assertEqual(response.status_code, 200)
-        data = analyse_response(response)
-        self.assertEqual(data.get('status'), 1) 
-
-    def test_0002_startsession_as_get(self):
+    def test_0001_startsession_error(self):
         """
         Test Start Session with Wrong-Request-Method
         """
@@ -34,12 +25,26 @@ class TestSession(TestCase):
         data = analyse_response(response)
         self.assertEqual(data.get('status'), -1)
 
+    def test_0002_checksession_error(self):
+        """
+        Test Check Session
+        """
+        token = 'hello world'
+        response = self.client.get('/session/check', data={'token' : token})
+        self.assertEqual(response.status_code, 200)
+        data = analyse_response(response)
+        self.assertEqual(data.get('status'), 0)
+
     def test_0003_checksession(self):
         """
         Test Start and Check Session
         """
-        token = SessionHelper.get_session_by_id(1).get('token')
+        response = self.client.post('/session/start')
+        self.assertEqual(response.status_code, 200)
+        data = analyse_response(response)
+        self.assertEqual(data.get('status'), 1)
+        token = data.get('data').get('token')
         response = self.client.get('/session/check', data={'token' : token})
         self.assertEqual(response.status_code, 200)
         data = analyse_response(response)
-        self.assertEqual(data.get('msg'), 'Success')
+        self.assertEqual(data.get('status'), 1)
