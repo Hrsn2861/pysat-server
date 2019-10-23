1bb0f939eb4038cb91a50f1ab7bbe4cf2ec7fad0
陈旭 <2017011341@secoder.net>
2019-10-23 14:52:04 +0800

    feat: submit program

--- program/urls/user.py
+++ program/urls/user.py
@@ -1,4 +1,16 @@
 """pysat URL Configuration for Program.User
 """
+from django.urls import path
+
+from utils.views import view_maker
+from utils.params import ParamType
+from utils.permission import ActionType
+from program.views import user
+
 urlpatterns = [
+    path('submit', view_maker(user.submit, 'POST', [
+        ParamType.ProgramName,
+        ParamType.ProgramCode,
+        ParamType.ProgramDoc
+    ], action=ActionType.SubmitProgram)),
 ]
--- program/views/user.py
+++ program/views/user.py
@@ -1,2 +1,16 @@
 """views for program.user
 """
+import utils.response as Response
+from utils.params import ParamType
+from program.models import ProgramHelper
+
+def submit(package):
+    """process the request of submitting program
+    """
+    user = package.get('user')
+    params = package.get('params')
+    program_name = params.get(ParamType.ProgramName)
+    program_code = params.get(ParamType.ProgramCode)
+    program_doc = params.get(ParamType.ProgramDoc)
+    ProgramHelper.add_program(user['id'], program_name, program_code, program_doc)
+    return Response.checked_response('Submit Success')
