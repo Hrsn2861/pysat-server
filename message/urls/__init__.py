c62a839bb958c41769730d7607eeae0e9c501347
陈旭 <2017011341@secoder.net>
2019-10-23 21:59:13 +0800

    feat: add the function of chat block

--- message/urls/__init__.py
+++ message/urls/__init__.py
@@ -4,8 +4,10 @@ from django.conf.urls import url, include
 
 from message.urls import message
 from message.urls import chat
+from message.urls import block
 
 urlpatterns = [
     url(r'^message/', include(message)),
-    url(r'^chat/', include(chat))
+    url(r'^chat/', include(chat)),
+    url(r'^block/', include(block))
 ]
