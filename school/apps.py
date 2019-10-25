fac5d4fa2477df5374dabdab639daee05a2fb812
陈旭 <2017011341@secoder.net>
2019-10-26 00:58:23 +0800

    feat: add school models

---
+++ school/apps.py
@@ -0,0 +1,8 @@
+"""School APP
+"""
+from django.apps import AppConfig
+
+class SchoolConfig(AppConfig):
+    """School Config
+    """
+    name = 'school'
---
+++ school/urls.py
@@ -0,0 +1,4 @@
+"""pysat URL Configuration for School
+"""
+urlpatterns = [
+]
