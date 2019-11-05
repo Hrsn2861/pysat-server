3533eb56fd0df1a00d44dd51ef0a6dc425699629
陈浩展 <chz17@mails.tsinghua.edu.cn>
2019-11-05 12:29:01 +0800

    test: add test for prgram user download and like

--- program/tests/test_proguser.py
+++ program/tests/test_proguser.py
@@ -3,6 +3,7 @@
 from django.test import TestCase
 
 from school.models import SchoolHelper, SubjectHelper
+from program.models import ProgramHelper
 from utils.tests.initialization import Initialization
 from utils.response import analyse_response
 
@@ -39,11 +40,73 @@ class TestProgramUserByRequest(TestCase):
 
         response = self.client.post('/program/user/submit', {
             'token' : self.token,
-            'code_name'  : 'Test Program',
+            'code_name'  : 'TestProgram',
             'code_content' : 'print("Hello World")',
             'code_readme' : 'Hello World',
             'school_id' : school_id,
             'theme_id' : theme_id
         })
+        data = analyse_response(response)
+        self.assertEqual(data.get('status'), 1)
+
+    def test_0002(self):
+        """this is a test of like
+        """
+        school = SchoolHelper.get_school_by_name('测试大学')
+        school_id = school.get('id')
+        theme = SubjectHelper.get_subject_by_name('计算机科学与技术')
+        theme_id = theme.get('id')
+
+        response = self.client.post('/program/user/submit', {
+            'token' : self.token,
+            'code_name'  : 'TestProgram',
+            'code_content' : 'print("Hello World")',
+            'code_readme' : 'Hello World',
+            'school_id' : school_id,
+            'theme_id' : theme_id
+        })
+        data = analyse_response(response)
+        self.assertEqual(data.get('status'), 1)
+
+        program = ProgramHelper.get_program_by_name('TestProgram')
+        code_id = program.get('id')
+
+        response = self.client.post('/program/user/like', {
+            'token' : self.token,
+            'code_id' : code_id
+        })
+
+        data = analyse_response(response)
+        self.assertEqual(data.get('msg'), 'Program not valid')
+    
+    def test_0003(self):
+        """this is a test of user download
+        """
+        """this is a test of like
+        """
+        school = SchoolHelper.get_school_by_name('测试大学')
+        school_id = school.get('id')
+        theme = SubjectHelper.get_subject_by_name('计算机科学与技术')
+        theme_id = theme.get('id')
+
+        response = self.client.post('/program/user/submit', {
+            'token' : self.token,
+            'code_name'  : 'TestProgram',
+            'code_content' : 'print("Hello World")',
+            'code_readme' : 'Hello World',
+            'school_id' : school_id,
+            'theme_id' : theme_id
+        })
+        data = analyse_response(response)
+        self.assertEqual(data.get('status'), 1)
+
+        program = ProgramHelper.get_program_by_name('TestProgram')
+        code_id = program.get('id')
+
+        response = self.client.get('/program/user/download', {
+            'token' : self.token,
+            'code_id' : code_id
+        })
+
         data = analyse_response(response)
         self.assertEqual(data.get('status'), 1)
