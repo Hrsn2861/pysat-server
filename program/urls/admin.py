7e26135cb40d02191b236d7992c22daa7b8368eb
陈浩展 <chz17@mails.tsinghua.edu.cn>
2019-11-06 10:16:20 +0800

    refactor: fix style test in program admin urls

--- program/urls/admin.py
+++ program/urls/admin.py
@@ -4,7 +4,6 @@ from django.urls import path
 
 from utils.views import view_maker
 from utils.params import ParamType
-from utils.permission import ActionType
 from program.views import admin
 
 urlpatterns = [
