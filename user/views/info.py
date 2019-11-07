a882a8e79ae2ae2e9768a1956df677320655955b
陈浩展 <chz17@mails.tsinghua.edu.cn>
2019-11-07 11:30:53 +0800

    fix: fix a bug in user info modify

--- user/views/info.py
+++ user/views/info.py
@@ -123,9 +123,10 @@ def modify_info(package):
         if target_public_permission >= public_permission:        #超级用户也不能修改root权限
             return Response.error_response('Access Denied: Can\'t modify your superior')
         if target_private_permission == 4:
-            return Response.error_response(
-                'Modify Denied: Cannot Demote or Promote Headmaster Here'
-                )
+            if modify_private_permission is not None:
+                return Response.error_response(
+                    'Modify Denied: Cannot Demote or Promote Headmaster Here'
+                    )
         if target_schoolid == 0 and modify_private_permission is not None:
             return Response.error_response(
                 'Access Denied: Cannot Modify Schoolless User\'s private permission'
